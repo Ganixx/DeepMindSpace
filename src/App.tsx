c92071d4df3ba225274094b9232dd8f8895650cc
Ganixx <ganeshyep@gmail.com>
2025-06-11 15:20:27 -0500

    Intial Setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Dashboard from './components/Dashboard'
+import Feauture from './components/Feauture'
+import Faq from './components/Faq'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+      <Feauture />
+      <Faq />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Feauture.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Feauture() {
+  return (
+    <div>Feauture</div>
+  )
+}
+
+export default Feauture
