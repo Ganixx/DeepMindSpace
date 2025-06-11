84fc3014b6a77cde8f92d6f0c03c73e83f20be4e
Ganixx <ganeshyep@gmail.com>
2025-06-11 17:59:58 -0500

    Feauture page changes

--- src/components/Feauture.tsx
+++ src/components/Feauture.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
 function Feauture() {
   return (
-    <div>Feauture</div>
+    <main className='bg-gray-100 h-dvh'>
+      <img src='/images/Wavy.png' className='w-full'/>
+      <div className='flex justify-center items-center h-8'>
+        <h1 className='text-5xl font-semibold text-black border-b border-black/40 pb-1'>
+          From Uncertainty to Clarity
+        </h1>
+      </div>
+    </main>
   )
 }
 
